bc958e6646f55a516c92973d18f509fb270051da
Maity <smaity9@dxc.com>
2019-10-18 17:16:19 +0530

    dist changes for json api

--- src/app/item/items.service.ts
+++ src/app/item/items.service.ts
@@ -17,7 +17,7 @@ export class ItemsService {
     });
 
     return this.http
-      .get("https://raj2912.github.io/Shopping/dist/shopping/assets/data.json", { headers: headers })
+      .get("https://api.myjson.com/bins/grhbs", { headers: headers })
       .toPromise()
       .then(res => res.json())
       .catch(err => {
